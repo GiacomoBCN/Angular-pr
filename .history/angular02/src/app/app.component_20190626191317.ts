ecbaf7d73c1b76355f39c9d998df7a5d8bfdd5c1
Giacomobcn <giacomobianchi05@gmail.com>
2019-06-26 19:18:50 +0200

    com angular02 pulsanti

---
+++ .history/angular02/src/app/app.component_20190626191317.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular02';
+  disable = false;
+  textoSaludar = "Este boston esta activado";
+  textoDisactivo = "Este botton no funciona ahora";
+  texstoActivar="activar botton saludo";
+  disabled = false;
+  disabledOff = true;
+  botonVerde() {
+    this.disabled = this.disabled;
+
+  }
+
+
+mostrarAviso(){
+alert('has pulsdo el boton');
+}
+
+activarSludo(){
+this.texstoActivar="este botton està habilitado",
+this.b
+
+}
+
+disactivarSaluo(){
+
+}
+
+}
