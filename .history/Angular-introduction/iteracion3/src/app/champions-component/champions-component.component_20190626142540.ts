8058bf63ae39e377c923b770eca81686c50ec658
Giacomobcn <giacomobianchi05@gmail.com>
2019-06-26 19:05:39 +0200

    ma ?

---
+++ .history/Angular-introduction/iteracion3/src/app/champions-component/champions-component.component_20190626142540.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-champions-component',
+  templateUrl: './champions-component.component.html',
+  styleUrls: ['./champions-component.component.scss']
+})
+export class ChampionsComponentComponent implements OnInit {
+championArr
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ .history/Angular-introduction/iteracion3/src/app/champions-component/champions-component.component_20190626142708.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-champions-component',
+  templateUrl: './champions-component.component.html',
+  styleUrls: ['./champions-component.component.scss']
+})
+export class ChampionsComponentComponent implements OnInit {
+championArray =[
+{
+
+}
+]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
